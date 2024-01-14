4322230f6ee4a794d6d1752088e3911321332bd3
Marces23 <marc.braun@students.fhnw.ch>
2024-01-14 17:19:10 +0100

    Zwischenstand

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
-app.use(express.json());
-
 const app = express();
 const port = 3000;
+
+const url =
+  'http://192.168.0.27:8123/api/states/button.gledopto_gl_mc_001p_identify';
+let headers = {
+  Authorization:
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NjkxMjU3NDIzMDg0YWEyODliZDk1NzlkODZlYTBmMCIsImlhdCI6MTcwNTE0NDU5NiwiZXhwIjoyMDIwNTA0NTk2fQ.VaVUPHWmKdx3-byNxfBuNbtzd8IIQ8SwcI57JaP4Ex4',
+  'content-type': 'application/json'
+};
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello');
@@ -11,10 +19,10 @@ app.get('/', (req, res) => {
 
 app.get('/echo', (req, res) => {
   const cmd = req.query.comand ?? 'command not found';
-  
+
   res.send({
-    command: cmd,
-    });
+    command: cmd
+  });
 });
 
 app.listen(port, () => {
@@ -22,29 +30,18 @@ app.listen(port, () => {
 });
 
 app.get('/state', (req, res) => {
-  response = app.get(url, headers=headers)
-  
+  response = app.get(url, headers);
+
   res.send({
-    state: response.text,
-    });
+    state: response.text
+  });
 });
 
-url = "http://192.168.0.27:8123/api/states/button.gledopto_gl_mc_001p_identify"
-headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NjkxMjU3NDIzMDg0YWEyODliZDk1NzlkODZlYTBmMCIsImlhdCI6MTcwNTE0NDU5NiwiZXhwIjoyMDIwNTA0NTk2fQ.VaVUPHWmKdx3-byNxfBuNbtzd8IIQ8SwcI57JaP4Ex4",
-    "content-type": "application/json",
-}
-
-response = app.get(url, headers=headers)
-print(response.text)
-
-
-app.post('/command', (req, res) => {
+/* app.post('/command', (req, res) => {
   const { body } = req;
   console.log(body);
   //commands.push(body);
   res.send({
     status: 'ok'
-  })
-});
-
+  });
+}); */
