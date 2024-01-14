7870d0381ddde759a78879c45ba85cada26f332d
Marces23 <79634707+Marces23@users.noreply.github.com>
2024-01-14 18:27:03 +0100

    FetchState

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const url =
+/* const url =
   'http://192.168.0.27:8123/api/states/button.gledopto_gl_mc_001p_identify';
 let headers = {
   Authorization:
     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NjkxMjU3NDIzMDg0YWEyODliZDk1NzlkODZlYTBmMCIsImlhdCI6MTcwNTE0NDU5NiwiZXhwIjoyMDIwNTA0NTk2fQ.VaVUPHWmKdx3-byNxfBuNbtzd8IIQ8SwcI57JaP4Ex4',
   'content-type': 'application/json'
-};
+}; */
 
 app.use(express.json());
 
@@ -30,12 +30,35 @@ app.listen(port, () => {
 });
 
 app.get('/state', (req, res) => {
-  response = app.get(url, headers);
-
+  state = getState();
   res.send({
     state: response.text
   });
 });
+
+async function getState() {
+  fetch(
+    'http://192.168.0.27:8123/api/states/button.gledopto_gl_mc_001p_identify',
+    {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NjkxMjU3NDIzMDg0YWEyODliZDk1NzlkODZlYTBmMCIsImlhdCI6MTcwNTE0NDU5NiwiZXhwIjoyMDIwNTA0NTk2fQ.VaVUPHWmKdx3-byNxfBuNbtzd8IIQ8SwcI57JaP4Ex4',
+        'content-type': 'application/json'
+      }
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // handle the response data here
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      // handle any errors here
+      console.error(error);
+    });
+}
 
 /* app.post('/command', (req, res) => {
   const { body } = req;
